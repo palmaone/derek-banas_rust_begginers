d1a53f7b60d26d537bd319f45ab2956b8b19a9bf
Israel Palma <isra.palma@gmail.com>
2023-02-20 09:56:05 -0600

    Basic data types and math

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,22 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    const ONE_MIL: u32 = 1_000_000;
-    const PI: f32 = 3.141592;
-    /*
-     * shadowing 
-     * using 2 or more variables with same name
-     *  but with different data types 
-     */
-    let age = "47";
-    let mut age: u32 = age.trim().parse()
-    .expect("Age wasn't assigned a number");
+    //Unsigned integers: u8, u16, u32, u64, u128, usize
+    //Signed integers: i8, i16, i32, i64, i128, isize
+    println!("Max u32 : {}", u32::MAX);
+    println!("Max u64 : {}", u64::MAX);
+    println!("Max u128 : {}", u128::MAX);
+    println!("Max usize : {}", usize::MAX);
+    println!("Max f32 : {}", f32::MAX);
 
-    age += 1;
+    let _is_true = true; //allow unused by prepending an underscore
+    let is_true = true;
+    let my_grade = 'A';
 
-    println!("I'm {} and I want ${}", age, ONE_MIL);
+    let num_1: f32 = 1.111111111111111;
+    println!("f32:{}", num_1 + 0.111111111111111);
+
+    let num_2: f64 = 1.111111111111111;
+    println!("f64:{}", num_2 + 0.111111111111111);
+    
 }
