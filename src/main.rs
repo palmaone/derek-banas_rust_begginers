bba732d7b4b6d3686a38b407846a5bcaecacdaaf
Israel Palma <isra.palma@gmail.com>
2023-02-20 12:22:37 -0600

    The match desicion

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,20 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    let random_num = rand::thread_rng().gen_range(1..101);
-    println!("Random: {}", random_num);
-
-    let mut my_age = 47;
     
-    let can_vote = if my_age >= 18 {
-        true //return true
-    } else { false };
-
-    println!("Can vote:{}", can_vote)
+    let age = 0;
+    match age {
+        1..=18 => println!("Important Bday!"),
+        21 | 50 => println!("Important Bday!"),
+        65..=i32::MAX => println!("Important Bday!"),
+        _ => println!("NOT AN Important Bday!"),//Default Bday
+    }
     
+    let my_age = 18;
+    let voting_age = 18;
+    match my_age.cmp(&voting_age) {//.cmp is the compare function
+        Ordering::Less => println!("NO, Can't vote!"),
+        Ordering::Greater => println!("YES, Can vote!"),
+        Ordering::Equal => println!("YES, Congrats NOW YOU Can vote!"),
+    }
 }
