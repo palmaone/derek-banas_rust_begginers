db4a721a654568daf1e1ca9a552664bc36e4d24e
Israel Palma <isra.palma@gmail.com>
2023-02-20 12:32:12 -0600

    Arrays and loops

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,31 @@ use std::cmp::Ordering;
 
 fn main() {
     
-    let age = 0;
-    match age {
-        1..=18 => println!("Important Bday!"),
-        21 | 50 => println!("Important Bday!"),
-        65..=i32::MAX => println!("Important Bday!"),
-        _ => println!("NOT AN Important Bday!"),//Default Bday
-    }
-    
-    let my_age = 18;
-    let voting_age = 18;
-    match my_age.cmp(&voting_age) {//.cmp is the compare function
-        Ordering::Less => println!("NO, Can't vote!"),
-        Ordering::Greater => println!("YES, Can vote!"),
-        Ordering::Equal => println!("YES, Congrats NOW YOU Can vote!"),
+    let arr_1 = [1,2,3,4];
+    // println!("1st: {}", arr_1[0]);
+    // println!("Len: {}", arr_1.len());
+
+    let arr_2 = [1,2,3,4,5,6,7,8,9];
+    let mut loop_idx = 0;
+
+    // loop{
+    //     if arr_2[loop_idx] % 2 == 0 {
+    //         loop_idx+=1;
+    //         continue;
+    //     }
+    //     if arr_2[loop_idx] == 9{
+    //         break;
+    //     }
+    //     println!("Val: {}", arr_2[loop_idx]);
+    //     loop_idx+=1;
+    // }
+
+    // while loop_idx<arr_2.len() {
+    //     println!("Arr: {}",arr_2[loop_idx]);
+    //     loop_idx+=1;
+    // }
+
+    for val in arr_2.iter(){
+        println!("Val : {}", val);
     }
 }
