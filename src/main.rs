1509b546ba93eeebdef73a84dc451d2e64bf3cb8
Israel Palma <isra.palma@gmail.com>
2023-02-20 09:45:28 -0600

    About mutable variables

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,18 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("What is your name?");
-    let mut name = String::new();
-    let greeting = "Nice to meet you";
-    io::stdin().read_line(&mut name)
-    .expect("Didn't receive input");
-    println!("Hello, {}! {}", name.trim_end(), greeting);
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    /*
+     * shadowing 
+     * using 2 or more variables with same name
+     *  but with different data types 
+     */
+    let age = "47";
+    let mut age: u32 = age.trim().parse()
+    .expect("Age wasn't assigned a number");
+
+    age += 1;
+
+    println!("I'm {} and I want ${}", age, ONE_MIL);
 }
